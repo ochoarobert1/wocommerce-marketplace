0791fc33b8a067e34b231291270bc1865b6b254c
Robert Ochoa <ochoa.robert1@gmail.com>
2022-04-15 14:59:12 -0400

    Agregado los Node_modules al gitignore

--- includes/class-woocommerce-marketplace.php
+++ includes/class-woocommerce-marketplace.php
@@ -172,6 +172,7 @@ class Woocommerce_Marketplace {
 
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_styles' );
 		$this->loader->add_action( 'wp_enqueue_scripts', $plugin_public, 'enqueue_scripts' );
+		$this->loader->add_action( 'woocommerce_before_add_to_cart_button', $plugin_public, 'woomarketplace_add_start_date', 10, 0 ); 
 
 	}
 
--- public/class-woocommerce-marketplace-public.php
+++ public/class-woocommerce-marketplace-public.php
@@ -20,84 +20,99 @@
  * @subpackage Woocommerce_Marketplace/public
  * @author     Robert Ochoa <ochoa.robert1@gmail.com>
  */
-class Woocommerce_Marketplace_Public {
+class Woocommerce_Marketplace_Public
+{
 
-	/**
-	 * The ID of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $plugin_name    The ID of this plugin.
-	 */
-	private $plugin_name;
+    /**
+     * The ID of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $plugin_name    The ID of this plugin.
+     */
+    private $plugin_name;
 
-	/**
-	 * The version of this plugin.
-	 *
-	 * @since    1.0.0
-	 * @access   private
-	 * @var      string    $version    The current version of this plugin.
-	 */
-	private $version;
+    /**
+     * The version of this plugin.
+     *
+     * @since    1.0.0
+     * @access   private
+     * @var      string    $version    The current version of this plugin.
+     */
+    private $version;
 
-	/**
-	 * Initialize the class and set its properties.
-	 *
-	 * @since    1.0.0
-	 * @param      string    $plugin_name       The name of the plugin.
-	 * @param      string    $version    The version of this plugin.
-	 */
-	public function __construct( $plugin_name, $version ) {
+    /**
+     * Initialize the class and set its properties.
+     *
+     * @since    1.0.0
+     * @param      string    $plugin_name       The name of the plugin.
+     * @param      string    $version    The version of this plugin.
+     */
+    public function __construct($plugin_name, $version)
+    {
+        $this->plugin_name = $plugin_name;
+        $this->version = $version;
+    }
 
-		$this->plugin_name = $plugin_name;
-		$this->version = $version;
+    /**
+     * Register the stylesheets for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_styles()
+    {
 
-	}
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Woocommerce_Marketplace_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Woocommerce_Marketplace_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-	/**
-	 * Register the stylesheets for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_styles() {
+        wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'css/woocommerce-marketplace-public.css', array(), $this->version, 'all');
+    }
 
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Woocommerce_Marketplace_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Woocommerce_Marketplace_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
+    /**
+     * Register the JavaScript for the public-facing side of the site.
+     *
+     * @since    1.0.0
+     */
+    public function enqueue_scripts()
+    {
 
-		wp_enqueue_style( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'css/woocommerce-marketplace-public.css', array(), $this->version, 'all' );
+        /**
+         * This function is provided for demonstration purposes only.
+         *
+         * An instance of this class should be passed to the run() function
+         * defined in Woocommerce_Marketplace_Loader as all of the hooks are defined
+         * in that particular class.
+         *
+         * The Woocommerce_Marketplace_Loader will then create the relationship
+         * between the defined hooks and the functions defined in this
+         * class.
+         */
 
-	}
+        wp_enqueue_script($this->plugin_name, plugin_dir_url(__FILE__) . 'js/woocommerce-marketplace-public.js', array( 'jquery' ), $this->version, false);
+    }
 
-	/**
-	 * Register the JavaScript for the public-facing side of the site.
-	 *
-	 * @since    1.0.0
-	 */
-	public function enqueue_scripts() {
-
-		/**
-		 * This function is provided for demonstration purposes only.
-		 *
-		 * An instance of this class should be passed to the run() function
-		 * defined in Woocommerce_Marketplace_Loader as all of the hooks are defined
-		 * in that particular class.
-		 *
-		 * The Woocommerce_Marketplace_Loader will then create the relationship
-		 * between the defined hooks and the functions defined in this
-		 * class.
-		 */
-
-		wp_enqueue_script( $this->plugin_name, plugin_dir_url( __FILE__ ) . 'js/woocommerce-marketplace-public.js', array( 'jquery' ), $this->version, false );
-
-	}
-
+    
+    /**
+     * Method woomarketplace_add_start_date
+     *
+     * @return void
+     */
+    public function woomarketplace_add_start_date()
+    {
+        woocommerce_form_field('ads_start_date', array(
+            'type'        => 'text',
+            'required'    => true,
+            'label'       => 'Ads Start Date',
+            'description' => 'Please select the starting point to show this ad',
+        ), '');
+    }
 }
